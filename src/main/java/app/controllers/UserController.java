26412218eb636525503dda3db5bee97c7735d58e
Zuhami <Aymanchaykh6@gmail.com>
2025-03-26 10:39:18 +0100

    login stuffv5

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,21 +1,40 @@
 package app.controllers;
 
+import app.entities.Task;
+import app.entities.User;
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.TaskMapper;
+import app.persistence.UserMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.List;
+
 public class UserController {
-    public static void addRoutes(Javalin app) {
-    app.post("/login", ctx ->login(ctx));
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+    app.post("/login", ctx ->login(ctx,connectionPool));
     }
-    public static void login(Context ctx){
+    public static void login(Context ctx, ConnectionPool connectionPool) {
         //hent form parametre
         String username= ctx.formParam("username");
         String password = ctx.formParam("password");
 
         // check om bruger findes i DB med de angivne username + password
+        try {
+            User user = UserMapper.login(username, password,connectionPool);
+            ctx.sessionAttribute("currentUser", user);
+            //hvis ja send videre til task siden
+            List<Task> taskList = TaskMapper.getAllTasksPerUser(user.getUserId(),connectionPool);
+            ctx.attribute("taskList", taskList);
+            ctx.render ("task.html");
+        } catch (DatabaseException e) {
+            //hvis nej send tilbage til login siden med fejl besked
+            ctx.attribute("message", e.getMessage());
+            ctx.render("index.html");
+        }
 
-        //hvis nej send tilbage til login siden med fejl besked
-        //hvis ja send videre til task siden
-        ctx.render ("task.html");
+
+
     }
 }
