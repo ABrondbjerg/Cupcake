f518e14b6ee0daf20f64ab9f8f97f6b35af3af23
LegitOscar <oscarboerjesen@gmail.com>
2025-03-31 10:55:59 +0200

    OAKA

--- src/main/java/app/entities/Cupcake.java
+++ src/main/java/app/entities/Cupcake.java
@@ -1,36 +1,38 @@
 package app.entities;
 
-public class Cupcake {
-
-    private String top;
-    private String bottom;
-    private String price;
 
 
-    public Cupcake(String top, String bottom, String price) {
-        this.top = top;
-        this.bottom = bottom;
-        this.price = price;
+
+    public class Cupcake {
+        private int quantity;
+        private String topName;
+        private String bottomName;
+        private double totalPrice;
+
+        public Cupcake(int quantity, String topName, String bottomName, double totalPrice) {
+            this.quantity = quantity;
+            this.topName = topName;
+            this.bottomName = bottomName;
+            this.totalPrice = totalPrice;
+        }
+
+        @Override
+        public String toString() {
+            return "Cupcake{" +
+                    "quantity=" + quantity +
+                    ", topName='" + topName + '\'' +
+                    ", bottomName='" + bottomName + '\'' +
+                    ", totalPrice=" + totalPrice +
+                    '}';
+        }
+
+        public int getQuantity() { return quantity; }
+        public String getTopName() { return topName; }
+        public String getBottomName() { return bottomName; }
+        public double getTotalPrice() { return totalPrice; }
     }
 
-    public String getTop() {
-        return top;
-    }
 
-    public String getBottom() {
-        return bottom;
-    }
 
-    public String getPrice() {
-        return price;
-    }
 
-    @Override
-    public String toString() {
-        return "Cupcake{" +
-                "top='" + top + '\'' +
-                ", bottom='" + bottom + '\'' +
-                ", price='" + price + '\'' +
-                '}';
-    }
-}
+
